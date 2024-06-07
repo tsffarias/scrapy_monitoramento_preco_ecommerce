7a6ccddbbdaaf802bf91f5c5c521459f3fae47a8
Thiago Silva Farias <thiago_protect@hotmail.com>
2024-06-07 14:00:55 -0300

    Atualizar dependências e corrigir configuração de build

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Atualize os pacotes e instale dependências do sistema
-sudo apt-get update -y
-sudo apt-get install -y build-essential python3-dev gcc libffi-dev
-
-# Instale as dependências Python
-pip install -r requirements.txt
